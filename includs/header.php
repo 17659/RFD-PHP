3848dffffb76626d381c2988655873f8eee04c3d
Exposito Esteban <17659@csmrouen.net>
2019-02-12 14:11:29 +0100

    Commitation d'après-midi.

---
+++ includs/header.php
@@ -0,0 +1,10 @@
+<hearder>
+    <nav>
+        <ul>
+            <li><a href=""index.php?page="accueil"></a></li>
+            <li><a href=""index.php?page="produits"></a></li>
+            <li><a href=""index.php?page="inscription"></a></li>
+            <li><a href=""index.php?page="contact"></a></li>
+        </ul>
+    </nav>
+</hearder>
